accc53a5c9e545c225ac48741d0fbf8eabc6c5c3
GenXoutsourcing <95140975+GenXoutsourcing@users.noreply.github.com>
2025-12-08 12:41:01 -0500

    Update certbot.sh

--- certbot.sh
+++ certbot.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 mv /etc/httpd/conf.d/viciportal-ssl.conf /etc/httpd/conf.d/viciportal-ssl.conf.
-sudo systemctl stop firewalld
+systemctl stop firewalld
 certbot renew
-sudo systemctl restart firewalld
+systemctl restart firewalld
 mv /etc/httpd/conf.d/viciportal-ssl.conf. /etc/httpd/conf.d/viciportal-ssl.conf
 domain=$(basename /etc/letsencrypt/renewal/*.conf | sed 's/\.conf$//')
 systemctl reload httpd
-asterisk -rx "reload"
+/usr/sbin/asterisk -rx "reload"
