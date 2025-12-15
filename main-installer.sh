fc62a37173b0e4ac08d8123cf0e7fd879b1934d7
GenXoutsourcing <95140975+GenXoutsourcing@users.noreply.github.com>
2025-12-15 14:56:54 -0500

    Update main-installer.sh

--- main-installer.sh
+++ main-installer.sh
@@ -378,7 +378,7 @@ echo 'Continuing...'
 echo "Installing astguiclient"
 mkdir /usr/src/astguiclient
 cd /usr/src/astguiclient
-svn checkout svn://svn.eflo.net/agc_2-X/trunk
+svn checkout svn://svn.eflo.net/agc_2-X/trunk -r 3956
 cd /usr/src/astguiclient/trunk
 
 #Add mysql users and Databases
